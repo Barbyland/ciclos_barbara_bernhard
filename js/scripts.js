55fc77c1af27a3e6389ffe13a263c111c41b1faf
barbara.bernhard <barby_bernhard@hotmail.com>
2022-08-16 23:00:01 -0300

    sweet alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+//sweet alert//
+const btn = document.querySelector('#myBtn')
+ 
+btn.addEventListener('click', () => {
+ 
+    Swal.fire({
+        title: 'Genial!',
+        text: 'Haz clickeado el botón!',
+        icon: 'success',
+        confirmButtonText: 'Cool'
+    })
+})
+
+
 alert ("Descubre los artistas y generos musicales escuchados por las mujeres ") 
 
 let IngresarGenero = prompt("Ingresá tu genero Femenino/Masculino");
